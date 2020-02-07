22d87714585cc9d3f9b2298d3451049f2e8e5772
Ancientshield <ancientsword520@gmail.com>
2020-02-07 17:25:34 +0800

    Step06 & Step07: create rails model, basic CRUD, and let localhost root display task list

--- Gemfile
+++ Gemfile
@@ -23,12 +23,17 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
 # Reduces boot times through caching; required in config/boot.rb
-gem 'bootsnap', '>= 1.4.2', require: false
+# gem 'bootsnap', '>= 1.4.2', require: false
+gem 'bootsnap', '~> 1.4', '>= 1.4.5'
+gem 'bootstrap', '~> 4.4', '>= 4.4.1'
+gem 'sprockets-rails', '~> 3.2', '>= 3.2.1'
+gem 'jquery-rails', '~> 4.3', '>= 4.3.5'
+gem 'popper_js', '~> 1.16'
+gem 'simple_form', '~> 5.0', '>= 5.0.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path, notice: '任務新增成功！'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: '編輯成功！'
+    else
+    render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find_by(id: params[:id])
+    @task.destroy if @task
+    redirect_to tasks_path, notice: '任務已刪除！'
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+end

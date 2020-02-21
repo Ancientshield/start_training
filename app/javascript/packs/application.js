7bb83453456898169ce7f64e44b2dc764ccbd1bc
Ancientshield <ancientsword520@gmail.com>
2020-02-21 18:32:54 +0800

    resolve Travis CI problem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,6 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-import "path to application.css";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

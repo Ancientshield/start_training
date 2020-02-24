3072c4850e7fafa160a64038f5346d7e97851d26
Ancientshield <ancientsword520@gmail.com>
2020-02-24 16:26:39 +0800

    CI test

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,9 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  # 不使用 FactoryBot.build 會報錯
-  # undefined method `build' for #<RSpec::ExampleGroups::Task:0x00007faab6d8f6e0>
-  let(:invalid_task) { FactoryBot.build(:task, title: nil) }
+  let(:invalid_task) { build(:task, title: nil) }
 
   it 'should not valid' do
     expect(invalid_task).not_to be_valid

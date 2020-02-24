d2b3dc0692cf5eeb3150ce675fb0e49382660462
Ancientshield <ancientsword520@gmail.com>
2020-02-24 20:07:24 +0800

    Step08&Step09 Restore Travis CI and add  database test

--- spec/features/create_task_spec.rb
+++ spec/features/create_task_spec.rb
@@ -5,10 +5,11 @@ require 'rails_helper'
 RSpec.feature 'create task', type: :feature do
   scenario 'valid inputs' do
     visit new_task_path
-    fill_in (I18n.t :task_title).to_s, with: 'feature test title'
+    fill_in (I18n.t :task_title).to_s, with: 'Feature Test 001'
     click_on (I18n.t :sure).to_s
     visit tasks_path
-    expect(page).to have_content('feature test title')
+    expect(page).to have_content('Feature Test 001')
+    expect(Task.last.title).to eq 'Feature Test 001'
   end
 
   scenario 'invalid inputs' do
--- spec/features/delete_task_spec.rb
+++ spec/features/delete_task_spec.rb
@@ -4,10 +4,11 @@ require 'rails_helper'
 
 RSpec.describe 'delete task', type: :feature do
   scenario 'success' do
-    Task.create!(title: 'Feature Test Task')
+    Task.create!(title: 'Feature Test 002')
     visit tasks_path
-    expect(page).to have_content('Feature Test Task')
+    expect(page).to have_content('Feature Test 002')
     click_on (I18n.t :delete_task).to_s
-    expect(page).not_to have_content('Feature Test Task')
+    expect(page).not_to have_content('Feature Test 002')
+    expect(Task.last).to eq nil
   end
 end

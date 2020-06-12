81e65cb93c926a23a398e9daf1e8dd19466a1b83
Ancientsword <jacklin2100@gmail.com>
2020-06-12 23:40:12 +0800

    modify i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authorize
+  before_action :authorize, :set_locale
+  around_action :switch_locale
   def set_locale
     I18n.locale = extract_locale_from_tld || I18n.default_locale
   end
@@ -9,6 +10,29 @@ class ApplicationController < ActionController::Base
   def extract_locale_from_tld
     parsed_locale = request.host.split('.').last
     I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+
+  # 設定語系
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  # 語系切換
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  # 使用者選擇語系後可持續在其他頁面使用該語系
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
+
+  # 從子網域取得locale code
+  def extract_locale_from_tld
+    parsed_locale = request.subdomains.first
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ?
+    parsed_locale : nil
   end
 
   private
